472eeb53b66947d90bf566f7274df57781696779
test <test@test>
2022-12-12 16:35:54 +0800

    Added checking on private key

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -3,11 +3,8 @@ package burp;
 import java.io.PrintWriter;
 import java.util.List;
 import java.io.File;
-
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
-
 import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -51,41 +48,41 @@ public class BurpExtender implements burp.IBurpExtender, burp.IHttpListener
     private String key_path;
     private String resbody;
 
-    public static final String ALGO_RSA = "RSA";
-    public static final String ALGO_RSA_INSTANCE = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
-    public static final String ALGO_RSA_DIGEST = "SHA-256";
-    public static final String ALGO_RSA_MASK = "MGF1";
-    public static final String ALGO_EC = "EC";
-    public static final String SIGNATURE_ALGO_EC = "SHA256withECDSA";
-    public static final String ALGO_AES = "AES";
-    public static final String ALGO_AES_INSTANCE = "AES/GCM/NoPadding";
-    public static final int AES_KEY_LENGTH = 256;
-    public static final int AES_TAG_LENGTH = 128;
-    public static final int AES_IV_LENGTH_96 = 96;
-    public static final int AES_IV_LENGTH_12 = 12;
-    public static final String SIGNATURE = "SHA256withRSA";
-    public static final String PUBLIC_KEY_STRING_START = "-----BEGIN PUBLIC KEY-----";
-    public static final String PUBLIC_KEY_STRING_END = "-----END PUBLIC KEY-----";
-    public static final String PRIVATE_KEY_STRING_START = "-----BEGIN PRIVATE KEY-----";
-    public static final String PRIVATE_KEY_STRING_END = "-----END PRIVATE KEY-----";
-    public static final String HASHICORP_ROLE_ID = "HASHICORP_ROLE_ID";
-    public static final String HASHICORP_SECRET_ID = "HASHICORP_SECRET_ID";
-    public static final String HASHICORP_TOKEN = "HASHICORP_TOKEN";
-    public static final String HASHICORP_CLIENT_KEYSTORE_PWD = "HASHICORP_CLIENT_KEYSTORE_PWD";
-    public static final String HASHICORP_CLIENT_TRUSTSTORE_PWD = "HASHICORP_CLIENT_TRUSTSTORE_PWD";
-    public static final String HASHICORP_KEY_PREFIX = "vault:v1:";
-    public static final String HASHICORP_TRANSITE_SIGN_PATH = "transit/sign/";
-    public static final String HASHICORP_TRANSITE_VERIFY_PATH = "transit/verify/";
-    public static final String HASHICORP_TRANSITE_ENCRYPT_PATH = "transit/encrypt/";
-    public static final String HASHICORP_TRANSITE_DECRYPT_PATH = "transit/decrypt/";
-    public static final String HASHICORP_SECRET_SIGN_PATH = "secret/sign/";
-    public static final String HASHICORP_SECRET_VERIFY_PATH = "secret/verify/";
-    public static final String HASHICORP_ETHEREUM_ACCOUNT_ENDPOINT = "ethereum/accounts/";
-    public static final String SIGNATURE_ALGORITHM = "pkcs1v15";
-    public static final String HASH_ALGORITHM = "sha2-256";
-    public static final String UTF_8 = "UTF-8";
-    public static final String PROCESSING_UNBOUND_SIGN_REQUEST = "PROCESSING_UNBOUND_SIGN_REQUEST";
-    public static final String UNBOUND_SIGN_ERROR = "ERROR_UNBOUND_SIGN";
+    public String ALGO_RSA = "RSA";
+    public String ALGO_RSA_INSTANCE = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+    public String ALGO_RSA_DIGEST = "SHA-256";
+    public String ALGO_RSA_MASK = "MGF1";
+    public String ALGO_EC = "EC";
+    public String SIGNATURE_ALGO_EC = "SHA256withECDSA";
+    public String ALGO_AES = "AES";
+    public String ALGO_AES_INSTANCE = "AES/GCM/NoPadding";
+    public int AES_KEY_LENGTH = 256;
+    public int AES_TAG_LENGTH = 128;
+    public int AES_IV_LENGTH_96 = 96;
+    public int AES_IV_LENGTH_12 = 12;
+    public String SIGNATURE = "SHA256withRSA";
+    public String PUBLIC_KEY_STRING_START = "-----BEGIN PUBLIC KEY-----";
+    public String PUBLIC_KEY_STRING_END = "-----END PUBLIC KEY-----";
+    public String PRIVATE_KEY_STRING_START = "-----BEGIN PRIVATE KEY-----";
+    public String PRIVATE_KEY_STRING_END = "-----END PRIVATE KEY-----";
+    public String HASHICORP_ROLE_ID = "HASHICORP_ROLE_ID";
+    public String HASHICORP_SECRET_ID = "HASHICORP_SECRET_ID";
+    public String HASHICORP_TOKEN = "HASHICORP_TOKEN";
+    public String HASHICORP_CLIENT_KEYSTORE_PWD = "HASHICORP_CLIENT_KEYSTORE_PWD";
+    public String HASHICORP_CLIENT_TRUSTSTORE_PWD = "HASHICORP_CLIENT_TRUSTSTORE_PWD";
+    public String HASHICORP_KEY_PREFIX = "vault:v1:";
+    public String HASHICORP_TRANSITE_SIGN_PATH = "transit/sign/";
+    public String HASHICORP_TRANSITE_VERIFY_PATH = "transit/verify/";
+    public String HASHICORP_TRANSITE_ENCRYPT_PATH = "transit/encrypt/";
+    public String HASHICORP_TRANSITE_DECRYPT_PATH = "transit/decrypt/";
+    public String HASHICORP_SECRET_SIGN_PATH = "secret/sign/";
+    public String HASHICORP_SECRET_VERIFY_PATH = "secret/verify/";
+    public String HASHICORP_ETHEREUM_ACCOUNT_ENDPOINT = "ethereum/accounts/";
+    public String SIGNATURE_ALGORITHM = "pkcs1v15";
+    public String HASH_ALGORITHM = "sha2-256";
+    public String UTF_8 = "UTF-8";
+    public String PROCESSING_UNBOUND_SIGN_REQUEST = "PROCESSING_UNBOUND_SIGN_REQUEST";
+    public String UNBOUND_SIGN_ERROR = "ERROR_UNBOUND_SIGN";
 
     // implement IBurpExtender
     @Override
