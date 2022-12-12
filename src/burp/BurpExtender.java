389ed1262458699bd2182df09d18535f4c7f0055
test <test@test>
2022-12-12 16:25:53 +0800

    Added checking on private key

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -7,13 +7,41 @@ import java.io.File;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.MGF1ParameterSpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Optional;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
+
 public class BurpExtender implements burp.IBurpExtender, burp.IHttpListener
 {
     private burp.IExtensionHelpers helpers;
     private PrintWriter stdout;
     private PrintWriter stderr;
     private Boolean DEBUG = Boolean.TRUE;
-    private SecurityUtils securityUtils;
     private String key_path_unix= "/tmp/keys/private-key.pk8";
     private String key_path_win= "c:\\private-key.pk8";
     private String key_path;
@@ -53,6 +81,97 @@ public class BurpExtender implements burp.IBurpExtender, burp.IHttpListener
                     stdout.println("File does not exist: " + key_path);
             }
         }
+    }
+
+    public String decryptWithRSA(String textToDecrypt, String charset, String privateKeyPath) throws Exception {
+
+        if(DEBUG){
+            stdout.println("DEBUG: decryptWithRSA ");
+            stdout.println("DEBUG: textToDecrypt= " + textToDecrypt);
+            stdout.println("DEBUG: charset= " + charset);
+            stdout.println("DEBUG: privateKeyPath= " + privateKeyPath);
+        }
+
+        PrivateKey key = getPrivateKeyFromPKCS8(Constants.ALGO_RSA, privateKeyPath);
+        return decrypt(Constants.ALGO_RSA, textToDecrypt, key, charset, null);
+    }
+
+    private byte[] read(ByteArrayInputStream byteArrayInputStream) throws IOException {
+
+        if(DEBUG){stdout.println("DEBUG: read");};
+        Reader reader = new InputStreamReader(byteArrayInputStream);
+        StringWriter writer = new StringWriter();
+        char[] buffer = new char[2048];
+        while (true) {
+            int amount = reader.read(buffer);
+            if (amount >= 0) {
+                writer.write(buffer, 0, amount);
+            } else {
+                return writer.toString().getBytes();
+            }
+        }
+    }
+
+    public PrivateKey encodePrivateKey(String privateKey, String algorithm) throws Exception {
+        if(DEBUG){stdout.println("DEBUG: encodePrivateKey");};
+        
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(privateKey.replace(Constants.PRIVATE_KEY_STRING_START, "").replace(Constants.PRIVATE_KEY_STRING_END, "").getBytes());
+        if (byteArrayInputStream == null || algorithm == null || algorithm.equalsIgnoreCase("")) {
+            return null;
+        }
+        KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+        byte[] encodeKey = read(byteArrayInputStream);
+        return keyFactory.generatePrivate(new PKCS8EncodedKeySpec(Base64.getMimeDecoder().decode(encodeKey)));
+    }
+
+    public PrivateKey getPrivateKeyFromPKCS8(String algorithm, String privateKeyPath) throws Exception {
+        if(DEBUG){stdout.println("DEBUG: getPrivateKeyFromPKCS8");};
+        String privateKey = FileIOUtil.readFromFilePath(privateKeyPath);
+        return encodePrivateKey(privateKey, algorithm);
+    }
+
+    public String decrypt(String algorithm, String textToDecrypt, Key key, String charset, byte[] iv) throws Exception {
+        Cipher cipher;
+        byte[] textToDecryptBytes = Base64.getMimeDecoder().decode(textToDecrypt);
+        if(DEBUG){stdout.println("DEBUG: decrypt");};
+
+        if (algorithm.equalsIgnoreCase(Constants.ALGO_RSA)) {
+            cipher = Cipher.getInstance(Constants.ALGO_RSA_INSTANCE);
+            cipher.init(2, key, new OAEPParameterSpec("SHA-256", Constants.ALGO_RSA_MASK, MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT));
+        } else if (algorithm.equalsIgnoreCase(Constants.ALGO_AES)) {
+            cipher = Cipher.getInstance(Constants.ALGO_AES_INSTANCE);
+            cipher.init(2, key, new GCMParameterSpec(128, iv));
+        } else {
+            cipher = Cipher.getInstance(algorithm);
+            cipher.init(2, key);
+        }
+        return new String(cipher.doFinal(textToDecryptBytes), charset);
+    }
+
+    public String decryptWithAES(String textToDecrypt, String charset, SecretKey key, byte[] iv) throws Exception {
+        return decrypt(Constants.ALGO_AES, textToDecrypt, key, charset, iv);
+    }
+
+    public byte[] detachIV(String decryptedKey) {
+
+        if(DEBUG){stdout.println("DEBUG: detachIV");};
+
+        byte[] iv = null;
+        String[] tokens = decryptedKey.split("\\|");
+        if (tokens.length > 1) {
+            iv = Base64.getDecoder().decode(tokens[0]);
+        }
+        return iv;
+    }
+
+    public byte[] detachSecretKeyAES(String decryptedKey) {
+        if(DEBUG){stdout.println("DEBUG: detachSecretKeyAES");};
+        byte[] decodedKey = null;
+        String[] tokens = decryptedKey.split("\\|");
+        if (tokens.length > 1) {
+            decodedKey = Base64.getDecoder().decode(tokens[1]);
+        }
+        return decodedKey;
     }
 
     // implement IHttpListener
@@ -107,14 +226,14 @@ public class BurpExtender implements burp.IBurpExtender, burp.IHttpListener
 
                 if(DEBUG){stdout.println("DEBUG: key_path= " + key_path);}
 
-                String decryptedKey = securityUtils.decryptWithRSA(encryptedSecretKey, "UTF-8", key_path);
+                String decryptedKey = decryptWithRSA(encryptedSecretKey, "UTF-8", key_path);
                 if(DEBUG){stdout.println("DEBUG: decryptedKey= " + decryptedKey);}
-                byte[] iv = securityUtils.detachIV(decryptedKey);
-                byte[] decodedKey = securityUtils.detachSecretKeyAES(decryptedKey);
+                byte[] iv = detachIV(decryptedKey);
+                byte[] decodedKey = detachSecretKeyAES(decryptedKey);
 
                 // decrypt the data using decrypted secret key
                 SecretKey key = new SecretKeySpec(decodedKey, 0, decodedKey.length, Constants.ALGO_AES);
-                String decryptedBody = securityUtils.decryptWithAES(encryptedData, "UTF-8", key, iv);
+                String decryptedBody = decryptWithAES(encryptedData, "UTF-8", key, iv);
                 if(DEBUG){stdout.println("DEBUG: decryptedBody= " + decryptedBody);}
 
                 //get the data
